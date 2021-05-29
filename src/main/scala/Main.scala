2cbf1e98121a902c69cbd05830636f43fc5026af
sarpong <abasimisarpong@rocketmail.com>
2021-05-29 14:33:38 +0100

    [SA] Streaming Effect

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,5 +1,22 @@
-import cats.effect.{IO, IOApp}
+import cats.effect.{IO, IOApp, Sync}
+import fs2.Stream
 
 object Main extends IOApp.Simple {
-  def run: IO[Unit] = ???
+
+  override def run: IO[Unit] = {
+    val firstStream = Stream(1,2,3).toList
+    val emitStream = Stream.emit(1, 1, 3, 4).toList
+    val repeatedStream = Stream.range(1, 4).repeat.take(6).toVector
+    val evalStream = Stream.eval[IO, String](IO("Hello"))
+
+    for {
+      _  <- IO.println(s"I am just a stream ${firstStream}")
+      _  <- IO.println(s"I emit some stream ${emitStream}")
+      _  <- IO.println(s"The type of the first Stream is ${firstStream.getClass}")
+      _  <- IO.println(s"The type of the second Stream is ${emitStream.getClass}")
+      _  <- IO.println(s"This is a repeating Stream ${repeatedStream}")
+      _  <- IO.println(evalStream)
+    } yield ()
+
+  }
 }
